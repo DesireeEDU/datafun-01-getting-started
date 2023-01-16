d9fa9d26915dbef26772185094ac29ded5989026
Desiree Thompson <thompson.desiree@outlook.com>
2023-01-15 22:51:55 -0500

    Optional Task 6

--- xtra_p1.py
+++ xtra_p1.py
@@ -1,5 +1,7 @@
+# xtra_p1.py
 """
-Optional bonus. See course site for details.
+Desiree Thompson 1/15/2023
+Creation of a gamebot for futher learning on module 1 lessons. 
 
 """
 
@@ -7,12 +9,12 @@ import random
 
 # Change the name below to a name of your choice
 
-name = "GameBot"
+name = "DesBot"
 
 # Fix the code below to print the name using an f-string
 
 print()
-print("Hello, I'm {name}, your gamebot.")
+print(f"Hello, I'm {name}, your gamebot.")
 print("Let's play an animal guessing game!")
 print("There are 3 animals: wolf, eagle, snake (a Python of course).")
 print("The wolf scares the eagle.")
@@ -26,7 +28,7 @@ print()
 # enter wolf, eagle, or snake.
 # Hint: use the input() function
 
-user_choice = "wolf"
+user_choice = input("Please select a wolf, eagle, or snake as your animal: ")
 
 # Now the bot will pick one
 buddy_choice = random.choice(["wolf", "eagle", "snake"])
@@ -37,7 +39,6 @@ print(f"You said {user_choice}.")
 print(f"I said {buddy_choice}.")
 print()
 
-
 # Now we need to compare the choices and determine the winner
 # Complete the logic to
 # compare the choices and print who won
@@ -45,10 +46,15 @@ print()
 if user_choice == buddy_choice:
     print("We tied!")
 
+if user_choice > buddy_choice:
+    print("You are the winner!")
+
+if buddy_choice > user_choice:
+    print(f"{name} is the winner!")
+
 
 # When you finish,
 # right-click on the code and select "Format Document"
-
 # Run the code, and play the game a few times.
-# Copy the output from the terminal and paste it into 
+# Copy the output from the terminal and paste it into
 # a new file named xtra_p1out.txt.
